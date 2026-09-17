e3cae6239b66d50cc697810f4b22b2c6a0470221
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-09-17 23:53:35 +0600

    Loading component created

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,17 @@
+import '../styles/loading.css'
+
+const Loading = () => {
+  return (
+    <div class="honeycomb">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,17 +1,16 @@
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 
-
 const Movies = () => {
-    return (
-        <>
-        <Navbar/>
-        <div className='text-5xl font-bold font-mono text-rose-950'>
-            This is movies page.
-        </div>
-        <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="text-5xl font-bold font-mono text-rose-950">
+        This is movies page.
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Movies;
