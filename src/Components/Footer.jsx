c4eda248e2438f5ea50b172c8406fb9af13cf5d7
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-09-17 23:41:27 +0600

    Home page is done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,21 @@
-
+import { MdOutlineMovie } from "react-icons/md";
 
 const Footer = () => {
-    return (
-        <footer>
-            THis is footer.
-        </footer>
-    );
+  return (
+    <footer className="bg-gray-100 rounded-base shadow-sm">
+      <div className="w-full mx-auto max-w-screen p-4 md:flex md:items-center md:justify-between">
+        <span className="text-md sm:text-center">
+          © 2026 MovieExplorer. All Rights Reserved.
+        </span>
+
+        <div className="flex gap-2 items-center text-xl md:text-2xl xl:text-3xl"> 
+          <MdOutlineMovie size={40} color="blue"/>
+          <span className="bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-bold">Movie Explorer</span>
+        </div>
+
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
-import Footer from '../Components/Footer';
-import Navbar from '../Components/Navbar'
+import { Link } from "react-router";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+import bgPic from "../assets/heroPic.jpg";
 
 const Home = () => {
-    return (
-        <>
-        <Navbar/>
-        <div className='text-5xl font-bold font-mono text-gray-950'>
-            This is Home page
+  return (
+    <>
+      <Navbar />
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url(${bgPic})`,
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">DISCOVER MOVIES</h1>
+            <p className="mb-5">
+              Every movie tells a story. Some make you laugh, some make you think, some take you to places you've never been, and some stay with you long after the credits roll. Discover the world of cinema, explore movies across different genres and countries, and find the next story worth giving your time to.
+            </p>
+            <Link to='/movies' className="btn btn-primary">Explore Now</Link>
+          </div>
         </div>
-        <Footer/>
-        </>
-    );
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
